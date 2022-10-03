15b3b7a11f07974171f03349fc8ade26d5538792
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2022-10-03 13:38:41 +0300

    Nix files

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "DendrETH";
+
+  # Opt into `nix-blockchain-development`'s substituter (binary cache).
+  # `nixConfig` settings are not transitive so every user of a flake with a
+  # custom binary cache must manually include its `nixConfig` settings for
+  # substituters and trusted public keys:
+  nixConfig = {
+    extra-substituters = "https://nix-blockchain-development.cachix.org";
+    extra-trusted-public-keys = "nix-blockchain-development.cachix.org-1:Ekei3RuW3Se+P/UIo6Q/oAgor/fVhFuuuX5jR8K/cdg=";
+  };
+
+  inputs = {
+    # To ensure all packages from mcl-blockchain will be fetched from its
+    # binary cache we need to ensure that we use exact same commit hash of the
+    # inputs below. If we didn't, we may either:
+    # * end up with multiple copies of the same package from nixpkgs
+    # * be unable to use the binary cache, since the packages there where
+    #   using different versions of their dependencies from nixpkgs
+    mcl-blockchain.url = "github:metacraft-labs/nix-blockchain-development";
+    nixpkgs.follows = "mcl-blockchain/nixpkgs";
+    flake-utils.follows = "mcl-blockchain/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    mcl-blockchain,
+  }:
+    flake-utils.lib.simpleFlake {
+      inherit self nixpkgs;
+      name = "cosmos-playground";
+      shell = ./shell.nix;
+      preOverlays = [mcl-blockchain.overlays.default];
+    };
+}
---
+++ shell.nix
@@ -0,0 +1,12 @@
+{pkgs}:
+with pkgs;
+  mkShell {
+    packages = [
+      metacraft-labs.cosmos-theta-testnet # <after>
+    ];
+    shellHook = ''
+      # export GAIAD_BOOSTTRAP_DATA_DIR=${metacraft-labs.cosmos-theta-testnet}/data
+
+      figlet "Cosmos Playground"
+    '';
+  }
