ed94dd3a512d2fe912bd04ff51f8c491fca4de4b
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2022-10-03 15:10:41 +0300

    Overrider `rust-bin` so we can use `target wasm32-unknown-unknown`

--- shell.nix
+++ shell.nix
@@ -1,12 +1,19 @@
-{pkgs}:
-with pkgs;
-  mkShell {
+{pkgs}: let
+  rustTargetWasm =
+    pkgs.rust-bin.stable.latest.default.override
+    {
+      extensions = ["rust-src"];
+      targets = ["wasm32-unknown-unknown"];
+    };
+in
+  pkgs.mkShell {
     packages = [
-      metacraft-labs.cosmos-theta-testnet # <after>
-      rust-bin.beta.latest.default
+      pkgs.metacraft-labs.cosmos-theta-testnet # <after>
+      rustTargetWasm
+      pkgs.go
     ];
     shellHook = ''
-      export GAIAD_BOOSTTRAP_DATA_DIR=${metacraft-labs.cosmos-theta-testnet}/data
+      export GAIAD_BOOSTTRAP_DATA_DIR=${pkgs.metacraft-labs.cosmos-theta-testnet}/data
 
       figlet "Cosmos Playground"
     '';
