f61e48a6c727d209ad7b7d94b9e19856776a8b90
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2022-10-11 17:57:02 +0300

    Add changes related to linking external lib

--- shell.nix
+++ shell.nix
@@ -7,6 +7,23 @@
     };
 
   wasmd = pkgs.callPackage ./nix/wasmd {};
+  emscripten-enriched-cache = pkgs.emscripten.overrideAttrs (old: {
+    postInstall = ''
+      pushd $TMPDIR
+      echo 'int __main_argc_argv() { return 42; }' >test.c
+      for MEM in "-s ALLOW_MEMORY_GROWTH" ""; do
+        for LTO in -flto ""; do
+          for OPT in "-O2" "-O3" "-Oz" "-Os"; do
+            for REALOC in "-s RELOCATABLE=1" ""; do
+              for NOENTRY in "--no-entry" ""; do
+                $out/bin/emcc $MEM $LTO $OPT $REALOC $NOENTRY -s WASM=1 -s STANDALONE_WASM test.c
+              done
+            done
+          done
+        done
+      done
+    '';
+  });
 in
   pkgs.mkShell {
     packages = [
@@ -17,6 +34,10 @@ in
       wasmd
 
       pkgs.jq
+
+      pkgs.binaryen
+
+      emscripten-enriched-cache
     ];
     shellHook = ''
       export GAIAD_BOOSTTRAP_DATA_DIR=${pkgs.metacraft-labs.cosmos-theta-testnet}/data
